dd624acef2028bbf0b7c4b707f47bde4e2779457
Ryan Mount <rmount96@outlook.com>
2020-11-11 15:58:35 -0600

    Blackjack-game

---
+++ bj-script.js
@@ -0,0 +1,373 @@
+let deck = [
+    {
+        name:"Ace of clubs",
+        val: 1,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/ace_of_clubs.png?raw=true"
+    },
+    {
+        name:"2 of clubs",
+        val: 2,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/2_of_clubs.png?raw=true"
+    },
+    {
+        name:"3 of clubs",
+        val: 3,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/3_of_clubs.png?raw=true"
+    },
+    {
+        name:"4 of clubs",
+        val: 4,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/4_of_clubs.png?raw=true"
+    },
+    {
+        name:"5 of clubs",
+        val: 5,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/5_of_clubs.png?raw=true"
+    },
+    {
+        name:"6 of clubs",
+        val: 6,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/6_of_clubs.png?raw=true"
+    },
+    {
+        name:"7 of clubs",
+        val: 7,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/7_of_clubs.png?raw=true"
+    },
+    {
+        name:"8 of clubs",
+        val: 8,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/8_of_clubs.png?raw=true"
+    },
+    {
+        name:"9 of clubs",
+        val: 9,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/9_of_clubs.png?raw=true"
+    },
+    {
+        name:"10 of clubs",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/10_of_clubs.png?raw=true"
+    },
+    {
+        name:"King of clubs",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/king_of_clubs.png?raw=true"
+    },
+    {
+        name:"Queen of clubs",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/queen_of_clubs.png?raw=true"
+    },
+    {
+        name:"Jack of clubs",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/jack_of_clubs.png?raw=true"
+    },
+    {
+        name: "Ace of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/ace_of_spades.png?raw=true"
+    },
+    {
+        name: "2 of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/2_of_spades.png?raw=true"
+    },
+    {
+        name: "3 of spades",
+        val: 3,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/3_of_spades.png?raw=true"
+    },
+    {
+        name: "4 of spades",
+        val: 4,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/4_of_spades.png?raw=true"
+    },
+    {
+        name: "5 of spades",
+        val: 5,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/5_of_spades.png?raw=true"
+    },
+    {
+        name: "6 of spades",
+        val: 6,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/6_of_spades.png?raw=true"
+    },
+    {
+        name: "7 of spades",
+        val: 7,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/7_of_spades.png?raw=true"
+    },
+    {
+        name: "8 of spades",
+        val: 8,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/8_of_spades.png?raw=true"
+    },
+    {
+        name: "9 of spades",
+        val: 9,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/9_of_spades.png?raw=true"
+    },
+    {
+        name: "10 of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/10_of_spades.png?raw=true"
+    },
+    {
+        name: "King of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/king_of_spades.png?raw=true"
+    },
+    {
+        name: "Queen of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/queen_of_spades.png?raw=true"
+    },
+    {
+        name: "Jack of spades",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/jack_of_spades.png?raw=true"
+    },
+    {
+        name: "Ace of Hearts",
+        val: 1,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/ace_of_hearts.png?raw=true"
+    },
+    {
+        name: "2 of Hearts",
+        val: 2,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/2_of_hearts.png?raw=true"
+    },
+    {
+        name: "3 of Hearts",
+        val: 3,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/3_of_hearts.png?raw=true"
+    },
+    {
+        name: "4 of Hearts",
+        val: 4,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/4_of_hearts.png?raw=true"
+    },
+    {
+        name: "5 of Hearts",
+        val: 5,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/5_of_hearts.png?raw=true"
+    },
+    {
+        name: "6 of Hearts",
+        val: 6,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/6_of_hearts.png?raw=true"
+    },
+    {
+        name: "7 of Hearts",
+        val: 7,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/7_of_hearts.png?raw=true"
+    },
+    {
+        name: "8 of Hearts",
+        val: 8,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/8_of_hearts.png?raw=true"
+    },
+    {
+        name: "9 of Hearts",
+        val: 9,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/9_of_hearts.png?raw=true"
+    },
+    {
+        name: "10 of Hearts",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/10_of_hearts.png?raw=true"
+    },
+    {
+        name: "King of Hearts",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/king_of_hearts.png?raw=true"
+    },
+    {
+        name: "Queen of Hearts",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/queen_of_hearts.png?raw=true"
+    },
+    {
+        name: "Jack of Hearts",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/jack_of_hearts.png?raw=true"
+    },
+    {
+        name: "Ace of Diamonds",
+        val: 1,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/ace_of_diamonds.png?raw=true"
+    },
+    {
+        name: "2 of Diamonds",
+        val: 2,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/2_of_diamonds.png?raw=true"
+    },
+    {
+        name: "3 of Diamonds",
+        val: 3,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/3_of_diamonds.png?raw=true"
+    },
+    {
+        name: "4 of Diamonds",
+        val: 4,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/4_of_diamonds.png?raw=true"
+    },
+    {
+        name: "5 of Diamonds",
+        val: 5,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/5_of_diamonds.png?raw=true"
+    },
+    {
+        name: "6 of Diamonds",
+        val: 6,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/6_of_diamonds.png?raw=true"
+    },
+    {
+        name: "7 of Diamonds",
+        val: 7,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/7_of_diamonds.png?raw=true"
+    },
+    {
+        name: "8 of Diamonds",
+        val: 8,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/8_of_diamonds.png?raw=true"
+    },
+    {
+        name: "9 of Diamonds",
+        val: 9,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/9_of_diamonds.png?raw=true"
+    },
+    {
+        name: "10 of Diamonds",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/10_of_diamonds.png?raw=true"
+    },
+    {
+        name: "King of Diamonds",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/king_of_diamonds.png?raw=true"
+    },
+    {
+        name: "Queen of Diamonds",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/queen_of_diamonds.png?raw=true"
+    },
+    {
+        name: "Jack of Diamonds",
+        val: 10,
+        picURL: "https://github.com/DigitalCraftsStudents/blackjack-exercise/blob/master/images/jack_of_diamonds.png?raw=true"
+    },
+
+]
+
+
+deck.forEach; {
+    i = 0; i < deck.length; i++
+}
+let points = deck[i].val
+
+
+let playerArray = []
+let dealerArray = []
+let playerValues = []
+let dealerValues = []
+playerArray.count = 0
+dealerArray.count = 0
+
+
+
+let deckCount = deck.length
+let dealButton = document.querySelector('#deal-button')
+let hitButton = document.querySelector('#hit-button')
+
+let dealerHand = document.querySelector('#dealer-hand')
+let playerHand = document.querySelector('#player-hand')
+let hand = document.querySelector('.hand')
+let playerPoints = document.querySelector('#player-points')
+let dealerPoints = document.querySelector('#dealer-points')
+
+//playerPoints = []
+//dealerPoints = 0
+
+
+const calculatePoints = function() {
+    let val = document.createElement('value')
+    for (let i = 0; i <= 4; i++) {
+        for (let j = 0; j <= 4; i++) {
+            let val = deck[i].val
+            let val2 = deck[j].val
+            let total = val + val2
+            playerPoints.append(total)
+        }
+    }
+}
+
+const deal = function() {
+        
+        for (let i = 0; i <= 4; i++) {
+            let img = document.createElement('img')
+            if (playerArray.length < 2) {
+                playerArray.push(deck[i])
+                //val = deck[i].val
+                //playerPoints.append(val)
+                img.src = deck[i].picURL
+                playerHand.append(img)
+                deckCount -= deck[i]
+                //calculatePoints()
+
+            }
+            else if (dealerArray.length < 2) {
+                dealerArray.push(deck[i])
+                val = deck[i].val
+                dealerPoints.append(val)
+                img.src = deck[i].picURL
+                dealerHand.append(img)
+                deckCount -= deck[i]
+                //calculatePoints()
+            }
+        }
+        console.log(playerArray, dealerArray)
+    }
+
+const hit = function() {
+    for (let i = 0; i < 53; i++) {
+        let img = document.createElement('img')
+        if (playerArray.length < 8) {
+            playerArray.push(deck[i].val)
+                img.src = deck[i].picURL
+                playerHand.append(img)
+        }   else if (dealerArray.length < 8) {
+            dealerArray.push(deck[i].val)
+                img.src = deck[i].picURL
+                dealerHand.append(img)
+        }
+    } endGame()
+}
+
+
+deck.shuffle = function() {
+    deckCount = 52;
+    for (let i = 51; i > 0; i--) {
+        let r = Math.floor((i+1)*Math.random(i));
+        let temp = deck[r];
+        deck[r] = deck[i];
+        deck[i] = temp;
+    }
+    deckCount--
+
+ }
+
+
+endGame = function() {
+    if (playerArray.val >= 21 || dealerArray.val >= 21) {
+        return false
+    }
+}
+
+
+dealButton.addEventListener('click', deck.shuffle)
+dealButton.addEventListener('click', deal)
+//dealButton.addEventListener('click', getPoints)
+hitButton.addEventListener('click', hit)
